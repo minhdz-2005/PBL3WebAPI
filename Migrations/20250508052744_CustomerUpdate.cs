663564eee77ed9f966b0c82f7ab2bac0352b635d
minhdz-2005 <minhphung2102@gmail.com>
2025-05-08 19:57:25 +0700

    Order col update

---
+++ Migrations/20250508052744_CustomerUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PBL3WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Customer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Models/Order.cs
+++ Models/Order.cs
@@ -14,7 +14,7 @@ public class Order {
     public int ShiftId { get; set; }
     [ForeignKey("Voucher")]
     public int VoucherId { get; set; }
-    public DateTime TimeAndDate = DateTime.Now;
+    public DateTime TimeAndDate { get; set; } = DateTime.Now;
     [Precision(18,2)]
     public decimal Amount { get; set; }
     [Precision(18,2)]
@@ -24,12 +24,11 @@ public class Order {
     public bool Status { get; set; }
 
     public Order () {}
-    public Order (int staffId, int customerId, int shiftId, int voucherId, DateTime timeAndDate, decimal amount, decimal discountValue, bool status) {
+    public Order (int staffId, int customerId, int shiftId, int voucherId, decimal amount, decimal discountValue, bool status) {
         StaffId = staffId;
         CustomerId = customerId;
         ShiftId = shiftId;
         VoucherId = voucherId;
-        TimeAndDate = timeAndDate;
         Amount = amount;
         DiscountValue = discountValue;
         FinalAmount = Amount - DiscountValue;
